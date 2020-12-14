92afcbde728a23cd5417eba6c627fb80f2520d95
RedoineReddad <reddad.redoine@gmail.com>
2020-12-14 17:22:26 +0100

    generated boilerplate for Vuejs client

---
+++ IAD-FlexSharks/projet/FrontEnd/client/src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Ping from '../components/Ping.vue';
+import Tickets from '../components/Tickets.vue';
+import Login from '../components/Login.vue';
+import Booked from '../components/Booked.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path: '/tickets/available',
+    name: 'Tickets',
+    component: Tickets,
+  },
+  {
+    path: '/tickets/booked',
+    name: 'Booked',
+    component: Booked,
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
